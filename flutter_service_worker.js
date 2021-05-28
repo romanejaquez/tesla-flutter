6cca5205813ebafabafc81b86015a779ab9abf0d
Roman Jaquez <romanejaquez@gmail.com>
2021-05-28 17:40:12 -0400

    more updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c15615a60ae5baa17f50f2c2a02c28fc",
-"index.html": "468d5caebbec3031a9ca55626ae80f1d",
-"/": "468d5caebbec3031a9ca55626ae80f1d",
-"main.dart.js": "7fa7a56efa20461e7a066c2949387847",
+"index.html": "6520f9bedcab29a2306d8242ff8487ce",
+"/": "6520f9bedcab29a2306d8242ff8487ce",
+"main.dart.js": "48c182987c2597e4d6bb8f7c5a2659ff",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
